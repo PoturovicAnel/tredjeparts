dc44735a6587b864865081db0d40bec0e4f315c4
Anel Poturovic <poturovianel@gmail.com>
2019-11-21 12:35:01 +0100

    added api for dad jokes and some styling

--- public/myjava.js
+++ public/myjava.js
@@ -1,7 +1,19 @@
-function myFunction(){
+function myChuckJoke(){
 fetch('http://api.icndb.com/jokes/random')
   .then(response => response.json())
   .then(data => document.getElementById('ChuckNorris').innerHTML = data.value.joke)
+
+}
+
+function myDadJoke(){
+  fetch('https://icanhazdadjoke.com/', {
+    method: 'get',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Accept': 'application/json'
+    },
+  }).then(res=>res.json())
+  .then(data => document.getElementById('DadJoke').innerHTML = data.joke)
 
 }
 
